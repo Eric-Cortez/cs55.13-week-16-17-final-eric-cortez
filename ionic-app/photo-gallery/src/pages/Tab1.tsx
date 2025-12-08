5e0614d812d62c236a3dc093aa5cf6da63cba70f
Eric-Cortez <ecrcortez@gmail.com>
2025-12-07 22:15:06 -0800

    Implement product listing in Tab1 component with data fetching from API

--- ionic-app/photo-gallery/src/pages/Tab1.tsx
+++ ionic-app/photo-gallery/src/pages/Tab1.tsx
@@ -1,22 +1,57 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonListHeader,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+import React, { useState, useEffect } from "react";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
+  const [dataset, setDataset] = useState<any[]>([]);
+
+  const dataURL =
+    "https://dev-srjc-fall-2025-cs-55-13.pantheonsite.io/wp-json/wp/v2/product";
+
+  useEffect(() => {
+    fetch(dataURL)
+      .then((response) => response.json())
+      .then((data) => setDataset(data));
+  }, []);
+  console.log(dataset[0]);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Products</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">List</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList id="thing-list">
+          <IonListHeader>Product List</IonListHeader>
+          {dataset.map((item, index) => (
+            <IonItem lines="inset" key={index}>
+              <IonLabel>
+                <h4>{item.acf.product_name}</h4>
+                <p>{item.acf.description}</p>
+                <p>SKU: {item.acf.sku}</p>
+                <p>Price: {item.acf.price}</p>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
